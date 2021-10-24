467ead9b1b4b036c5c1c2011bf08a56b4ce65783
atharva bhandvalkar <70326109+a-tharva@users.noreply.github.com>
2021-10-24 12:16:06 +0530

    Update hello_world.py

--- python/Hello world/hello_world.py
+++ python/Hello world/hello_world.py
@@ -1,8 +1,6 @@
 from random import randrange as rand
 from time import sleep
 
-
-
 def print_string(current_string, expected_character):
     while True:
         some_random_character = rand(0, 127)
@@ -11,10 +9,8 @@ def print_string(current_string, expected_character):
         if chr(some_random_character) == expected_character:
             break
 
-
 my_string = "Hello World"
 current_str = ""
-
 
 for letter in my_string:
     print_string(current_str, letter)
